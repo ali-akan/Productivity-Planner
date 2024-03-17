b875e9f80e56d2a644e03e683cb5ad98cdd43740
ali-akan <aali.akan@gmail.com>
2024-03-17 17:05:51 +0300

    bug fix (mui & router comp)

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-
-import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
 import theme from "./theme/theme";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
